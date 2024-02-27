dca2a2a4dbda52d819c1b8d171a8d63e7f5e4e30
Nsja2806 <nizambranom@unal.edu.co>
2024-02-26 20:04:15 -0500

    Arreglo Indentación

--- televisores/tv.py
+++ televisores/tv.py
@@ -9,7 +9,7 @@ class TV:
         self._control=None
         TV._numTV+=1
 
-    ##Def metodos Set % Get
+    ##Def metodos Set & Get
     def setMarca(self,marca):
         self._marca=marca
     
@@ -42,35 +42,35 @@ class TV:
     def getControl(self):
         return self._control
 
-@classmethod
-def getNumTV(cls):
-    return cls._numTV
+    @classmethod
+    def getNumTV(cls):
+        return cls._numTV
 
-@classmethod
-def setNumTV(cls, numTV):
-    cls._numTV=numTV
+    @classmethod
+    def setNumTV(cls, numTV):
+        cls._numTV=numTV
 
-def turnOn(self):
-    self._estado==True
+    def turnOn(self):
+        self._estado==True
 
-def turnOff(self):
-    self._estado==False
+    def turnOff(self):
+        self._estado==False
 
-def canalUp(self):
-    if self._estado==True and self._canal<120:
-        self._canal+=1
+    def canalUp(self):
+        if self._estado and self._canal<120:
+            self._canal+=1
 
-def canalDown(self):
-    if self._estado==True and self._canal >1:
-        self._canal-=1
+    def canalDown(self):
+        if self._estado and self._canal >1:
+            self._canal-=1
 
-def volumenUp(self):
-    if self._estado==True and self.volumen<7:
-        self._volumen+=1
+    def volumenUp(self):
+        if self._estado==True and self.volumen<7:
+            self._volumen+=1
 
-def volumenDown(self):
-    if self._estado==True and self.volumen>0:
-        self._volumen-=1
+    def volumenDown(self):
+        if self._estado==True and self.volumen>0:
+            self._volumen-=1
 
-def getEstado(self):
-    return self._estado
+    def getEstado(self):
+        return self._estado
